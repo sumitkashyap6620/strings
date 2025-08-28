f01ce8f1ddbbdbf145d841695a62584b763de1eb
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2025-08-28 12:11:54 +0530

    basics

--- solution.java
+++ solution.java
@@ -1,27 +1,27 @@
-class Solution {
-    public static int[][] transpose(int arr[][]){
-    for (int i = 0 ; i < arr.length ; i++){
-        for(int j = i+1 ; j < arr[0].length ; j++ ){
-             arr[i][j] = arr[j][i] + arr[i][j];
-             arr[j][i] = arr[i][j] - arr[j][i];
-             arr[i][j] = arr[i][j] - arr[j][i];
+import java.util.Scanner;
+
+public class solution {
+       public static void Display_matrix(int[][] arr){
+    for (int i  = 0 ; i < arr.length ; i++){
+    for(int j = 0 ; j < arr[i].length ; j++)
+    System.out.print(arr[i][j]+" ");   System.out.println();
+}
+}
+    public static void main(String[] args) { 
+    Scanner sc = new Scanner(System.in);
+    System.out.print("for pasclas triangle give Rows :-  ");
+    int r = sc.nextInt(); 
+    int arr[][] = new int[r][];
+    for (int i = 0 ; i < r ; i++  ){
+        arr[i] = new int[i+1];
+        arr[i][0] = 1;
+        arr[i][i] = 1 ;
+        for (int j = 1 ; j<i ; j++){
+            arr[i][j] = arr[i-1][j]+arr[i-1][j-1];
         }
-}
-return arr ;
-}
-    public void rotate(int[][] arr) {
-       arr = transpose(arr);
- for (int i = 0 ; i < arr.length ; i++){
-    int j = 0 ; 
-    int k = arr[0].length-1;
-    while(j!=k){
-        arr[i][j] = arr[i][j] + arr[i][k] ;
-        arr[i][k] = arr[i][j] - arr[i][k] ;
-         arr[i][j] = arr[i][j] - arr[i][k] ;
-         j++;
-         k--;
     }
- }
- 
+    System.out.println("pascals triangle :-");
+    Display_matrix(arr);
+    sc.close();
 }
-    }
+}
