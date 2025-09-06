0a503b46e9db09f9c2404e9c65a5d51fd9ce9f99
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2025-09-06 11:55:45 +0530

    using inbuild functions

--- basic.java
+++ basic.java
@@ -1,22 +1,41 @@
-import java.util.*;
 public class basic{
     public static void main(String args[]){
      String s = "sumit" ;
     // System.out.println(s);
 
-     Scanner sc = new Scanner(System.in);
+    // Scanner sc = new Scanner(System.in);
     //  String s = sc.nextLine();
 
     // System.out.println(s.charAt(4));
 
-   // System.out.println(s.length());
+    // System.out.println(s.length());
    
-  // System.out.println(s.contains("mit"));
+   // System.out.println(s.contains("mit"));
 
-  // System.out.println(s.startsWith("su"));
+   // System.out.println(s.startsWith("su"));
 
-  
-    }
+   // same ends with 
+
+   //   System.out.println(s.concat(" aachaji"));
+
+   //System.out.println(s+23+10);
+
+  //    System.out.println(s+(23+10));
+
+  // System.out.println(23+10+s);
+
+    // System.out.println(s.compareTo("tumit"));
+
+    // System.out.println(s.substring(2));
+
+    // System.out.println(s.substring(0,4));
+
+    // System.out.println(s.equals("sum"));
+
+    //ystem.out.println(s.indexOf("m"));
+
+    //System.out.println(s.indexOf("sumit"));
+ }
 }
 
   
---
+++ prashnt.java
@@ -0,0 +1,7 @@
+public class prashnt{
+    public static void main (String arg[]){
+       //System.out.println(("sumit")+(30+20));
+       System.out.print(" " + 30+20+"sumit");
+
+    }
+}
