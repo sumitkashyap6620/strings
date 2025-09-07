84097ce6b13a91dec6f1f09aa43902ddd927a7d3
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2025-09-07 23:27:36 +0530

    StringBuilder

--- SB.java
+++ SB.java
@@ -2,6 +2,16 @@ public class SB{
     public static void main(String args[]){
         StringBuilder sb = new StringBuilder("SUMIT KASTYAP");
         sb.setCharAt(9 , 'H');
+        sb.append(" "+9);
+        sb.append(9.9321521);
+        sb.append( "  "  +"sum");
+        sb.insert(4,4);
+        sb.deleteCharAt(4);
+        sb.delete(14,'u');
+        int x = 65;
+        sb.setCharAt(0, (char)x);
+        // System.out.println(sb.reverse());
         System.out.println(sb);
+        // MOST OF THE STRING FUNCTION ALSO AVAILABLE !
     }
 }
--- solution.java
+++ solution.java
@@ -1,27 +1,17 @@
-import java.util.Scanner;
-
-public class solution {
-       public static void Display_matrix(int[][] arr){
-    for (int i  = 0 ; i < arr.length ; i++){
-    for(int j = 0 ; j < arr[i].length ; j++)
-    System.out.print(arr[i][j]+" ");   System.out.println();
-}
-}
-    public static void main(String[] args) { 
-    Scanner sc = new Scanner(System.in);
-    System.out.print("for pasclas triangle give Rows :-  ");
-    int r = sc.nextInt(); 
-    int arr[][] = new int[r][];
-    for (int i = 0 ; i < r ; i++  ){
-        arr[i] = new int[i+1];
-        arr[i][0] = 1;
-        arr[i][i] = 1 ;
-        for (int j = 1 ; j<i ; j++){
-            arr[i][j] = arr[i-1][j]+arr[i-1][j-1];
+public class solution{
+ public String toLowerCase(String s) {
+        StringBuilder sb = new StringBuilder(s); 
+        for(int i = 0 ; i < sb.length() ; i++){
+            int x = (int)sb.charAt(i);
+            if(x <= 90){
+                x += 32 ; 
+                char c = (char) x ;
+                 sb.setCharAt(i,c);
+            }
         }
+        s = sb.toString() ;
+      return s ;
     }
-    System.out.println("pascals triangle :-");
-    Display_matrix(arr);
-    sc.close();
-}
-}
+    public static void main(String[] args) { 
+   
+}}
