48fbf78697f7dfa51910f3825f4093b56dc8c443
sumitkashyap6620 <sumit.kashyap.6620@gmail.com>
2025-09-17 23:09:48 +0530

    static keyword

--- Student.java
+++ Student.java
@@ -4,6 +4,10 @@ public class Student {
     int enrol ;
     int sr ;
     double percent ;
+
+    // Static keyword
+
+   private static int numberofstudent; // it create common block for all objects and it can be access bye class name also after static 
     
     // final keyword ]
 
@@ -16,9 +20,15 @@ public class Student {
      public Student(int roll ,String name){
         this.roll = roll ;
         this.name = name ;
+        numberofstudent++ ;
      }
 
     // getter function 
+
+    public static int getnumberofstudents(){
+        return numberofstudent;
+    }
+
 
     public  int getroll(){
         return roll ;
--- oops.java
+++ oops.java
@@ -1,11 +1,25 @@
 // user define datatypes are pass by value 
 public class oops{
     public static void main(String[] args) {
+       // Student.numberofstudent = 100 ;
         Student s = new Student(5,"sumit");
+      //  System.out.println(s.numberofstudent); // By object
+       // System.out.println(Student.numberofstudent); // By class 
+       System.out.println(s.getnumberofstudents());
+       System.out.println(Student.getnumberofstudents());
+
         // s.name = "sumit kashyap";
         // s.setroll(125);
         System.out.println(s.name);
-        System.out.print(s.getroll());
+        System.out.println(s.getroll());
+        Student s2 = new Student(15,"Amit");
+         System.out.println(s2.name);
+        System.out.println(s2.getroll());
+      //   System.out.println(s2.numberofstudent); // By object
+       //  System.out.println(Student.numberofstudent); // By class 
+       System.out.println(s2.getnumberofstudents());
+       System.out.println(s.getnumberofstudents());
+
     }
     
 }
